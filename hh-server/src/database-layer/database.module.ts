4543805da0173105ba65ddf88f55aadef7360ccc
Marko Urh <urh.marko@gmail.com>
2021-04-28 02:21:32 +0200

    conn

--- hh-server/src/database-layer/database.module.ts
+++ hh-server/src/database-layer/database.module.ts
@@ -3,6 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   // TODO: Factory conn string from somewhere
-  imports: [MongooseModule.forRoot('mongodb+srv://admin:admin@pricelydev.axkl5.mongodb.net/human-hours?retryWrites=true&w=majority')],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
 })
 export class DatabaseModule {}
